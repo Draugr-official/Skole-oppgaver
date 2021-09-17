bcf0819e77e42fd899ce93a8cc264c3d68fcbf20
NativeX64 <57600814+Draugr-official@users.noreply.github.com>
2021-09-17 13:32:08 +0200

    Add files via upload

---
+++ Databaseoppgave8-31-2021/SqlClient.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Databaseoppgave8_31_2021
+{
+    /// <summary>
+    /// SQL pointer client
+    /// </summary>
+    class SqlClient
+    {
+        
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="table"></param>
+        public SqlClient(string host, string username, string password, string table)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Sends a query to the local sql client with specified data
+        /// </summary>
+        /// <param name="script"></param>
+        /// <returns></returns>
+        public string[][] Query(string data)
+        {
+            return new string[][]{ };
+        }
+    }
+
+    /// <summary>
+    /// Standard SQL table with simple logic
+    /// </summary>
+    class SqlTable
+    {
+        /// <summary>
+        /// Gets / sets the items of the table
+        /// </summary>
+        public SqlColumns Items { get; set; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="Items"></param>
+        public SqlTable(SqlColumns Items = null)
+        {
+            if(Items != null)
+            {
+                this.Items = Items;
+            }
+            else
+            {
+                this.Items = new SqlColumns();
+            }
+        }
+    }
+
+    class SqlColumns : Dictionary<string, SqlColumn> { }
+
+    /// <summary>
+    /// Standard SQL column
+    /// </summary>
+    class SqlColumn
+    {
+        /// <summary>
+        /// Gets / sets the name of the column
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets / sets the row of the column
+        /// </summary>
+        public string[] Row { get; set; }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="Name"></param>
+        public SqlColumn(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
